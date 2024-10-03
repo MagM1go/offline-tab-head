c250a79e34d9e7c03a05c9729092192d539ac142
MagM1go <magmigo2@inbox.ru>
2024-10-03 21:01:53 +0800

    Initial commit

---
+++ src/main/java/com/magmigo/mixin/PlayerListHudMixin.java
@@ -0,0 +1,24 @@
+package com.magmigo.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerListHud.class)
+public class PlayerListHudMixin {
+	/*
+	Yes.
+	We just return true here, because minecraft checks server encrypting before rendering heads.
+	 */
+	@ModifyExpressionValue(
+			method = "render",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/network/ClientConnection;isEncrypted()Z"
+			)
+	)
+	private boolean onTabRender(boolean original) {
+		return true;
+	}
+}
